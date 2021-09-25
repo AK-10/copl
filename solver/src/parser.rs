0fac7066e241a0787a90d37da769b1fd133f58aa
AK-10 <atsushi524k10@gmail.com>
2021-09-25 18:31:19 +0900

    impl nested add

--- solver/src/parser.rs
+++ solver/src/parser.rs
@@ -10,7 +10,7 @@ pub enum Value {
 #[derive(Debug)]
 pub enum Prim {
     Add(Box<Expr>, Box<Expr>),
-    //Sub(Box<Expr>, Box<Expr>),
+    Sub(Box<Expr>, Box<Expr>),
     //Mul(Box<Expr>, Box<Expr>),
     //LessThan(Box<Expr>, Box<Expr>)
 }
@@ -23,28 +23,50 @@ pub enum Expr {
 }
 
 pub fn parse(tokens: &[Token]) -> anyhow::Result<Expr> {
-    let (expr, rest) = op(tokens)?;
+    let (expr, rest) = expr(tokens)?;
     match rest {
         [] => Ok(expr),
         _ => Err(anyhow::anyhow!("syntax error"))
     }
-
 }
-fn op(tokens: &[Token]) -> anyhow::Result<(Expr, &[Token])> {
-    let (exp, rest) = value(tokens)?;
+
+fn expr(tokens: &[Token]) -> anyhow::Result<(Expr, &[Token])> {
+    op1(tokens)
+}
+
+// 結合度が低いもの
+// +, -
+fn op1(tokens: &[Token]) -> anyhow::Result<(Expr, &[Token])> {
+    let (left, rest) = op2(tokens)?;
     match rest {
         [Token::Op(Operator::Plus), rest @ ..] => {
-            let (right, rest) = value(rest)?;
-            Ok((Expr::Prim(Prim::Add(box exp, box right)), rest))
-        }
-        [] => Err(anyhow::anyhow!("syntax error")),
-        _ => unimplemented!("")
+            let (right, rest) = op1(rest)?;
+            Ok((Expr::Prim(Prim::Add(box left, box right)), rest))
+        },
+        [Token::Op(Operator::Minus), rest @ ..] => {
+            let (right, rest) = op1(rest)?;
+            Ok((Expr::Prim(Prim::Sub(box left, box right)), rest))
+        },
+        _ => Ok((left, rest))
+    }
+
+
+
+}
+// 結合度が高いもの
+// +, -, <
+fn op2(tokens: &[Token]) -> anyhow::Result<(Expr, &[Token])> {
+    let (left, rest) = value(tokens)?;
+    match tokens {
+        [Token::Op(Operator::Mul), rest @ ..] => unimplemented!(""),
+        _ => Ok((left, rest))
     }
 }
 
 fn value(tokens: &[Token]) -> anyhow::Result<(Expr, &[Token])> {
     match tokens {
         [Token::Int(i), rest @ ..] => Ok((Expr::Value(Value::Int(*i)),rest)),
-        _ => unimplemented!()
+        [Token::Bool(i), rest @ ..] => Ok((Expr::Value(Value::Bool(*i)),rest)),
+        _ => op1(tokens)
     }
 }
