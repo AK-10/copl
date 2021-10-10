ac55a00c402c91831f98c4378d7c5c90f495d986
AK-10 <atsushi524k10@gmail.com>
2021-10-10 19:18:08 +0900

    fix lt apply_rule; solve q033

--- solver/src/solver.rs
+++ solver/src/solver.rs
@@ -169,9 +169,15 @@ fn eval(expr: &Expr) -> EvalResult {
             match eval(cond) {
                 EvalResult::Value(Value::Bool(b)) => {
                     if b {
-                        eval(then)
+                        match eval(then) {
+                            EvalResult::Err(_) => EvalResult::Err(EvalError::IfTError),
+                            res => res
+                        }
                     } else {
-                        eval(els)
+                        match eval(els) {
+                            EvalResult::Err(_) => EvalResult::Err(EvalError::IfTError),
+                            res => res
+                        }
                     }
                 }
                 EvalResult::Value(Value::Int(_)) => EvalResult::Err(EvalError::IfInt),
@@ -244,7 +250,7 @@ fn apply_rule(expr: &Expr) {
             println!("{} evalto {} by E-Lt {{", expr, v);
             apply_rule(l);
             apply_rule(r);
-            println!("{} times {} is {} by B-Lt {{}};", eval(l), eval(r), v);
+            println!("{} less than {} is {} by B-Lt {{}};", eval(l), eval(r), v);
             println!("}};");
         }
         (Expr::Prim(Prim::LessThan(l, r)), EvalResult::Err(e)) => {
@@ -276,62 +282,20 @@ fn apply_rule(expr: &Expr) {
             match e {
                 EvalError::IfError => apply_rule(cond),
                 EvalError::IfInt => apply_rule(cond),
-                EvalError::IfTError => apply_rule(then),
-                EvalError::IfFError => apply_rule(els),
-                _ => unreachable!("internal: unreachable point apply_rule: IfThenElse")
+                EvalError::IfTError => {
+                    apply_rule(cond);
+                    apply_rule(then);
+                }
+                EvalError::IfFError => {
+                    apply_rule(cond);
+                    apply_rule(els);
+                }
+                _ => {
+                    println!("{:?}", e);
+                    unreachable!("internal: unreachable point apply_rule: IfThenElse")
+                }
             }
             println!("}};");
         }
     }
-    //match expr {
-    //    Expr::Value(Value::Int(i)) => { println!("{} evalto {} by E-Int {{}};", i, eval(expr)); }
-    //    Expr::Value(Value::Bool(b)) => { println!("{} evalto {} by E-Bool {{}};", b, eval(expr)); }
-    //    Expr::Unary(Unary::Minus(_)) => { println!("{} evalto {} by E-Int {{}};", expr, eval(expr)); }
-    //    Expr::Prim(Prim::Add(l, r)) => {
-    //        let result = eval(expr);
-    //        println!("{} evalto {} by E-Plus {{", expr, result);
-    //        apply_rule(l);
-    //        apply_rule(r);
-    //        println!("{} plus {} is {} by B-Plus {{}};", eval(l), eval(r), result);
-    //        println!("}};");
-    //    }
-    //    Expr::Prim(Prim::Sub(l, r)) => {
-    //        let result = eval(expr);
-    //        println!("{} evalto {} by E-Minus {{", expr, result);
-    //        apply_rule(l);
-    //        apply_rule(r);
-    //        println!("{} minus {} is {} by B-Minus {{}};", eval(l), eval(r), result);
-    //        println!("}};");
-    //    }
-    //    Expr::Prim(Prim::Mul(l, r)) => {
-    //        let result = eval(expr);
-    //        println!("{} evalto {} by E-Times {{", expr, result);
-    //        apply_rule(l);
-    //        apply_rule(r);
-    //        println!("{} times {} is {} by B-Times {{}};", eval(l), eval(r), result);
-    //        println!("}};");
-    //    }
-    //    Expr::Prim(Prim::LessThan(l, r)) => {
-    //        let result = eval(expr);
-    //        println!("{} evalto {} by E-Lt {{", expr, result);
-    //        apply_rule(l);
-    //        apply_rule(r);
-    //        println!("{} less than {} is {} by B-Lt {{}};", eval(l), eval(r), result);
-    //        println!("}};");
-    //    }
-    //    Expr::IfThenElse(cond, then, els) => {
-    //        let result = eval(expr);
-    //        let cond_result = eval(cond);
-    //        if let EvalResult::Value(Value::Bool(true)) = cond_result {
-    //            println!("{} evalto {} by E-IfT {{", expr, result);
-    //            apply_rule(cond);
-    //            apply_rule(then);
-    //        } else {
-    //            println!("{} evalto {} by E-IfF {{", expr, result);
-    //            apply_rule(cond);
-    //            apply_rule(els);
-    //        }
-    //        println!("}};");
-    //    }
-    //}
 }
